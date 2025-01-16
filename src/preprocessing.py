146a27c3de67320df2b2df13771c2671820bada3
Jaime-Linares <jailinbar@alum.us.es>
2025-01-16 23:01:33 +0100

    [feat] more characteristics (from shots) obtained in processing the data from matches. WIP

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,6 +1,7 @@
 # encoding:utf-8
-import pandas as pd
 from src.fetch_data import get_events
+import pandas as pd
+import numpy as np
 
 
 
@@ -21,8 +22,9 @@ def process_all_matches(matches_df):
         away_team = match['away_team']
         winning_team ='home_team' if match['home_score'] > match['away_score'] else 'draw' if match['away_score'] == match['home_score'] else 'away_team'
         match_events = get_events(match_id)
+        match_events_sorted_by_index_df = match_events.sort_values(by=["index","type"])
         # convertimos toda la información del partido en métricas
-        match_metrics = _process_match(match_events, home_team, away_team, winning_team)
+        match_metrics = _process_match(match_events_sorted_by_index_df, home_team, away_team, winning_team)
         all_matches_metrics.append(match_metrics)
     
     return pd.DataFrame(all_matches_metrics)
@@ -40,15 +42,33 @@ def _process_match(events_df, home_team, away_team, winning_team):
         dict: A dictionary containing the processed match.    
     '''
     metrics = {
-        "total_shots_home": _calculate_num_event_type(events_df, home_team, 'Shot'),
-        "total_shots_away": _calculate_num_event_type(events_df, away_team, 'Shot'),
+        # estadísticas de generales
+        ## tiros
+        "total_shots_home": _num_event_type(events_df, home_team, 'Shot'),
+        "total_shots_away": _num_event_type(events_df, away_team, 'Shot'),
+        "shots_on_target_ratio_home": _ratio_shots_on_target(events_df, home_team),
+        "shots_on_target_ratio_away": _ratio_shots_on_target(events_df, away_team),
+        "average_shots_on_target_distance_home": _average_distance_to_goal_of_shots_on_target(events_df, home_team),
+        "average_shots_on_target_distance_away": _average_distance_to_goal_of_shots_on_target(events_df, away_team),
+        "shots_high_xG_home": _num_shots_high_xG(events_df, home_team),
+        "shots_high_xG_away": _num_shots_high_xG(events_df, away_team),
+        "shots_inside_area_home": _num_shots_inside_area(events_df, home_team),
+        "shots_inside_area_away": _num_shots_inside_area(events_df, away_team),
+        "shots_inside_area_ratio_home": _ratio_shots_inside_area(events_df, home_team),
+        "shots_inside_area_ratio_away": _ratio_shots_inside_area(events_df, away_team),
+        "shots_foot_home": _num_shots_with_body_part(events_df, home_team, "Foot"),
+        "shots_foot_away": _num_shots_with_body_part(events_df, away_team, "Foot"),
+        "shots_head_home": _num_shots_with_body_part(events_df, home_team, "Head"),
+        "shots_head_away": _num_shots_with_body_part(events_df, away_team, "Head"),
+        "shots_other_home": _num_shots_with_body_part(events_df, home_team, "Other"),
+        "shots_other_away": _num_shots_with_body_part(events_df, away_team, "Other"),
         "winning_team": winning_team,
     }
 
     return metrics
 
 
-def _calculate_num_event_type(events_df, team, event_type):
+def _num_event_type(events_df, team, event_type):
     '''
     Calculate the number of the event type selected for a specific team.
     params:
@@ -58,4 +78,107 @@ def _calculate_num_event_type(events_df, team, event_type):
         int: The number of shots.
     '''
     return events_df[(events_df['team'] == team) & (events_df['type'] == event_type)].shape[0]
+
+
+def _ratio_shots_on_target(events_df, team):
+    '''
+    Calculate the ratio of shots on target for a specific team.
+    params:
+        events_df (DataFrame): A DataFrame containing the events.
+        team (str): The team.
+    returns:
+        float: The ratio of shots on target.
+    '''
+    shots = _num_event_type(events_df, team, 'Shot')
+    shots_on_target = events_df[(events_df['team'] == team) & (events_df['type'] == 'Shot') & 
+                                (events_df['shot_outcome'].isin(["Goal","Saved","Saved To Post"]))].shape[0]
+
+    return shots_on_target / shots if shots > 0 else 0.0
+
+
+def _average_distance_to_goal_of_shots_on_target(events_df, team):
+    '''
+    Calculate the average distance to goal of shots on target for a specific team.
+    params:
+        events_df (DataFrame): A DataFrame containing the events.
+        team (str): The team.
+    returns:
+        float: The average distance to goal of shots on target.
+    '''
+    shots_on_target = events_df[(events_df['team'] == team) & (events_df['type'] == 'Shot') & 
+                                (events_df['shot_outcome'].isin(["Goal","Saved","Saved To Post"]))].copy()
+    shots_on_target = shots_on_target[shots_on_target['location'].notnull()]
+    # calculamos la distancia de cada disparo al centro de la portería
+    goal_x, goal_y = 120, 40  # coordenadas del centro de la portería
+    shots_on_target['distance_to_goal'] = np.sqrt(
+        (goal_x - shots_on_target['location'].str[0])**2 + 
+        (goal_y - shots_on_target['location'].str[1])**2
+    )
+
+    # calculamos la media de las distancias
+    average_distance = shots_on_target['distance_to_goal'].mean()
+    return average_distance
+
+
+def _num_shots_high_xG(events_df, team):
+    '''
+    Calculate the number of shots with a high xG for a specific team.
+    params:
+        events_df (DataFrame): A DataFrame containing the events.
+        team (str): The team.
+    returns:
+        int: The number of shots with a high xG.
+    '''
+    return events_df[(events_df['team'] == team) & (events_df['type'] == 'Shot') & 
+                     (events_df['shot_statsbomb_xg'] > 0.2)].shape[0]
+
+
+def _num_shots_inside_area(events_df, team):
+    '''
+    Calculate the number of shots inside the area for a specific team.
+    params:
+        events_df (DataFrame): A DataFrame containing the events.
+        team (str): The team.
+    returns:
+        int: The number of shots inside the area.
+    '''
+    return events_df[(events_df['team'] == team) & (events_df['type'] == 'Shot') &
+                     (events_df['location'].notnull()) &
+                     (events_df['location'].apply(lambda loc: isinstance(loc, list) and 102 <= loc[0] <= 120 and 18 <= loc[1] <= 62))].shape[0]
+                     
+
+def _ratio_shots_inside_area(events_df, team):
+    '''
+    Calculate the ratio of shots inside the area for a specific team.
+    params:
+        events_df (DataFrame): A DataFrame containing the events.
+        team (str): The team.
+    returns:
+        float: The ratio of shots inside the area.
+    '''
+    shots = _num_event_type(events_df, team, 'Shot')
+    shots_inside_area = _num_shots_inside_area(events_df, team)
+    return shots_inside_area / shots if shots > 0 else 0.0
+
+
+def _num_shots_with_body_part(events_df, team, body_part):
+    '''
+    Calculate the number of shots with a specific body part for a specific team.
+    params:
+        events_df (DataFrame): A DataFrame containing the events.
+        team (str): The team.
+        body_part (str): The body part.
+    returns:
+        int: The number of shots with a specific body part.
+    '''
+    num_shots = 0
+    if body_part == "Foot" :
+        num_shots = events_df[(events_df['team'] == team) & (events_df['type'] == 'Shot') & 
+                              (events_df['shot_body_part'].notnull()) &
+                              (events_df['shot_body_part'].isin(["Right Foot","Left Foot"]))].shape[0]
+    else:
+        num_shots = events_df[(events_df['team'] == team) & (events_df['type'] == 'Shot') & 
+                              (events_df['shot_body_part'].notnull()) &
+                              (events_df['shot_body_part'] == body_part)].shape[0]
+    return num_shots
 
