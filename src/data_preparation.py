9bce3125059967f0d6dcfe9cdf32da0fa08c6901
Jaime-Linares <jailinbar@alum.us.es>
2025-01-22 14:22:41 +0100

    [feat] reduce dataset dimensionality without lose information (shots)

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -53,3 +53,62 @@ def scale_data_train_test(X_train, X_test, scaler):
         raise ValueError('Invalid scaler. Valid scalers are: StandardScaler, MinMaxScaler')
     return X_train, X_test
 
+
+def reduce_dimensionality(matches_processed_df):
+    '''
+    Reduces the dimensionality of the data.
+    params:
+        matches_processed_df (DataFrame): A DataFrame containing the processed data.
+    returns:
+        DataFrame: A DataFrame containing the reduced data.
+    '''
+    matches_processed_reduced_df = _percentage_of_shots_home_team(matches_processed_df)
+    matches_processed_df = _percentage_of_shots_high_xG_home_team(matches_processed_reduced_df)
+    matches_processed_df = _percentage_of_shots_inside_area_home_team(matches_processed_reduced_df)
+    return matches_processed_reduced_df
+
+
+def _percentage_of_shots_home_team(matches_processed_df):
+    '''
+    Calculates the percentage of shots of the home team from all the match.
+    If there have been no shots in the match 0.5 for each team is assigned.
+    params:
+        matches_processed_df (DataFrame): A DataFrame containing the processed data.
+    returns:
+        DataFrame: A DataFrame containing the percentage of shots of the home team.
+    '''
+    total_shots = matches_processed_df['total_shots_home'] + matches_processed_df['total_shots_away']
+    matches_processed_df['percentage_shots_home'] = (matches_processed_df['total_shots_home'] / total_shots).fillna(0.5)
+    matches_processed_df.drop(['total_shots_home', 'total_shots_away'], axis=1, inplace=True)
+    return matches_processed_df
+
+
+def _percentage_of_shots_high_xG_home_team(matches_processed_df):
+    '''
+    Calculates the percentage of shots with high xG of the home team from all the match.
+    If there have been no shots with high xG in the match 0.5 for each team is assigned.
+    params:
+        matches_processed_df (DataFrame): A DataFrame containing the processed data.
+    returns:
+        DataFrame: A DataFrame containing the percentage of shots with high xG of the home team.
+    '''
+    total_shots_high_xG = matches_processed_df['shots_high_xG_home'] + matches_processed_df['shots_high_xG_away']
+    matches_processed_df['percentage_shots_high_xG_home'] = (matches_processed_df['shots_high_xG_home'] / total_shots_high_xG).fillna(0.5)
+    matches_processed_df.drop(['shots_high_xG_home', 'shots_high_xG_away'], axis=1, inplace=True)
+    return matches_processed_df
+
+
+def _percentage_of_shots_inside_area_home_team(matches_processed_df):
+    '''
+    Calculates the percentage of shots inside the area of the home team from all the match.
+    If there have been no shots inside the area in the match 0.5 for each team is assigned.
+    params:
+        matches_processed_df (DataFrame): A DataFrame containing the processed data.
+    returns:
+        DataFrame: A DataFrame containing the percentage of shots inside the area of the home team.
+    '''
+    total_shots_inside_area = matches_processed_df['shots_inside_area_home'] + matches_processed_df['shots_inside_area_away']
+    matches_processed_df['percentage_shots_inside_area_home'] = (matches_processed_df['shots_inside_area_home'] / total_shots_inside_area).fillna(0.5)
+    matches_processed_df.drop(['shots_inside_area_home', 'shots_inside_area_away'], axis=1, inplace=True)
+    return matches_processed_df
+
