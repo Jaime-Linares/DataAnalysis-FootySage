1cfcbb1647ebb8206a8d79f2173d5dca2e39dd33
Jaime-Linares <jailinbar@alum.us.es>
2025-01-23 13:54:52 +0100

    [refactor] removed unused functions

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,5 +1,5 @@
 # encoding:utf-8
-from sklearn.preprocessing import LabelEncoder, StandardScaler, MinMaxScaler
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 
 
@@ -19,7 +19,7 @@ def code_categorical_data_multiclass(processed_data):
 
 def divide_data_in_train_test(data, target, test_size=0.2):
     '''
-    Divides the data into training and test sets.
+    Divides the data into training and test sets with stratification.
     params:
         data (DataFrame): A DataFrame containing the data.
         target (DataFrame): A DataFrame containing the target.
@@ -29,29 +29,6 @@ def divide_data_in_train_test(data, target, test_size=0.2):
     '''
     X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=test_size, random_state=42, stratify=target)
     return X_train, X_test, y_train, y_test
-
-
-def scale_data_train_test(X_train, X_test, scaler):
-    '''
-    Scales the data.
-    params:
-        X_train (DataFrame): A DataFrame containing the training data.
-        X_test (DataFrame): A DataFrame containing the test data.
-        scaler (str): The scaler to use.
-    returns:
-        tuple: A tuple containing the scaled training and test data.
-    '''
-    if scaler == 'StandardScaler':
-        scaler = StandardScaler()
-        X_train = scaler.fit_transform(X_train)
-        X_test = scaler.transform(X_test)
-    elif scaler == 'MinMaxScaler':
-        scaler = MinMaxScaler()
-        X_train = scaler.fit_transform(X_train)
-        X_test = scaler.transform(X_test)
-    else:
-        raise ValueError('Invalid scaler. Valid scalers are: StandardScaler, MinMaxScaler')
-    return X_train, X_test
 
 
 # --- FUNCIÓN PRINCIPAL PARA REDUCIR LA DIMENSIONALIDAD ----------------------------------------------------------------------------------------------------------
