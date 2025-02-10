f21368a9225c14ef372785921e441fd2048d66c3
Jaime-Linares <jailinbar@alum.us.es>
2025-02-10 22:40:10 +0100

    [feat] implemented hyperparameter search with random distributions in DT MI model

--- src/experimentation.py
+++ src/experimentation.py
@@ -353,20 +353,20 @@ class ExperimentLauncher:
             ('classifier', DecisionTreeClassifier(random_state=42))
         ])
 
-        # definimos el espacio de búsqueda de hiperparámetros
-        param_grid = {
-            'classifier__max_depth': [2, 3, 5, 7, 9],
+        # definimos el espacio de búsqueda aleatoria de hiperparámetros
+        param_dist = {
+            'classifier__max_depth': stats.randint(2, 10),              
             'classifier__criterion': ['gini', 'entropy'],
-            'classifier__max_features': ['sqrt', 'log2', None]
+            'classifier__max_features': ['sqrt', 'log2', None]   
         }
 
-        # realizamos la búsqueda de hiperparámetros
+        # realizamos la búsqueda aleatoria de hiperparámetros
         skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-        grid_search = GridSearchCV(pipeline, param_grid, cv=skf, scoring='f1_macro', verbose=1, n_jobs=-1)
-        grid_search.fit(X_train_reduced, y_train)
+        random_search = RandomizedSearchCV(pipeline, param_dist, n_iter=100, cv=skf, scoring='f1_macro', verbose=1, n_jobs=-1, random_state=42)
+        random_search.fit(X_train_reduced, y_train)
 
         # mejores hiperparámetros
-        best_params_reduced = {k.replace('classifier__', ''): v for k, v in grid_search.best_params_.items()}
+        best_params_reduced = {k.replace('classifier__', ''): v for k, v in random_search.best_params_.items()}
         print("Best hyperparameters:", best_params_reduced)
         # mejor modelo reducido
         best_model_reduced = DecisionTreeClassifier(**best_params_reduced, random_state=42)
