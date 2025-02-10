580a7a50ca45f72f4be16cf0229fb84f41f76721
Jaime-Linares <jailinbar@alum.us.es>
2025-02-10 19:53:57 +0100

    [feat] implemented hyperparameter search with random distributions in RF oversampling model

--- src/experimentation.py
+++ src/experimentation.py
@@ -122,21 +122,21 @@ class ExperimentLauncher:
             ('classifier', RandomForestClassifier(random_state=42))
         ])
 
-        # definimos el espacio de búsqueda de hiperparámetros
-        param_grid = {
-            'classifier__n_estimators': [15, 20, 30, 40, 50, 60],
-            'classifier__max_depth': [3, 4, 5, 6],
+        # definimos el espacio de búsqueda de hiperparámetros con distribuciones aleatorias
+        param_dist = {
+            'classifier__n_estimators': stats.randint(10, 66),  
+            'classifier__max_depth': stats.randint(2, 7),       
             'classifier__criterion': ['gini', 'entropy'],
             'classifier__max_features': ['sqrt', 'log2', None]
         }
 
-        # realizamos la búsqueda de hiperparámetros
+        # realizamos la búsqueda aleatoria de hiperparámetros
         skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-        grid_search = GridSearchCV(pipeline, param_grid, cv=skf, scoring='f1_macro', verbose=1, n_jobs=-1)
-        grid_search.fit(X_train_resampled, y_train_resampled, classifier__sample_weight=sample_weights)
+        random_search = RandomizedSearchCV(pipeline, param_dist, n_iter=150, cv=skf, scoring='f1_macro', verbose=1, n_jobs=-1, random_state=42)
+        random_search.fit(X_train_resampled, y_train_resampled, classifier__sample_weight=sample_weights)
 
         # mejores hiperparámetros
-        best_params = {k.replace('classifier__', ''): v for k, v in grid_search.best_params_.items()}
+        best_params = {k.replace('classifier__', ''): v for k, v in random_search.best_params_.items()}
         print("Best hyperparameters:", best_params)
         # mejor modelo
         best_model = RandomForestClassifier(**best_params, random_state=42)
