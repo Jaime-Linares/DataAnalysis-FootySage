7acdcb79cb0cfb6e70ad34eb3459daf84511e045
Jaime-Linares <jailinbar@alum.us.es>
2025-02-11 13:00:34 +0100

    [feat] implemented hyperparameter search with random distributions in KNN model

--- src/experimentation.py
+++ src/experimentation.py
@@ -577,19 +577,19 @@ class ExperimentLauncher:
         ])
 
         # definimos el espacio de búsqueda de hiperparámetros
-        param_grid = {
-            'classifier__n_neighbors': [5, 7, 9, 11, 13, 15, 20, 25],
+        param_distributions = {
+            'classifier__n_neighbors': stats.randint(5, 26),
             'classifier__weights': ['distance'],
             'classifier__metric': ['euclidean', 'manhattan', 'minkowski', 'chebyshev']
         }
 
         # realizamos la búsqueda de hiperparámetros
         skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-        grid_search = GridSearchCV(pipeline, param_grid, cv=skf, scoring='f1_macro', verbose=1, n_jobs=-1)
-        grid_search.fit(X_train, y_train)
+        random_search = RandomizedSearchCV(pipeline, param_distributions, n_iter=30, cv=skf, scoring='f1_macro', verbose=1, n_jobs=-1, random_state=42)
+        random_search.fit(X_train, y_train)
 
         # mejores hiperparámetros
-        best_params = {k.replace('classifier__', ''): v for k, v in grid_search.best_params_.items()}
+        best_params = {k.replace('classifier__', ''): v for k, v in random_search.best_params_.items()}
         print("Best hyperparameters:", best_params)
         # mejor modelo
         X_train, X_test = scale_data_train_test(X_train, X_test, "minmax")
